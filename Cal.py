9ea5a24ca03b89843a3cc8285d3b67770db50c38
prasad123456789 <varaprasad_c@persistent.com>
2022-04-07 12:55:23 +0530

    3rd feature added

--- Cal.py
+++ Cal.py
@@ -1,9 +1,11 @@
 a=2
 b=10
 name="prasad"
+age=25
 if a+b==12:
     print("awesome");
 else: 
     print("howw......");
     print("How are you welocomr");
     print("hello my name is"+name);
+    print("student age"+age);
